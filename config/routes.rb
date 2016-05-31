f32b17383ba2cbd75b46f4dad5aec8fae120cbe2
mAlishera <rooh@mail.ru>
2016-05-31 19:08:03 +0300

    active admin with bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'venues#index'
 
   devise_for :consumers, controllers: { registrations: 'consumers/registrations', omniauth_callbacks: 'omniauth_callbacks', invitations: 'users/invitations' }
